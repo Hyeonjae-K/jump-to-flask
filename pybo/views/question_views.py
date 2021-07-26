757e3df2604b61039b525f7a1b473d349b5a8711
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-07-26 15:57:10 +0900

    3-02

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -12,8 +12,12 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list/')
 def _list():
+    # GET 방식으로 요청한 URL에서 page값을 가져옴
+    page = request.args.get('page', type=int, default=1)
     # 작성일시 역순(최신순)으로 질문 목록 조회
     question_list = Question.query.order_by(Question.create_date.desc())
+    # 조회한 question_list에 페이징 적용(조회할 페이지, 페이지당 보여줄 게시물)
+    question_list = question_list.paginate(page, per_page=10)
     # 조회된 질문 목록을 템플릿으로 전달
     return render_template('question/question_list.html', question_list=question_list)
 
